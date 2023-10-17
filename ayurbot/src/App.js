5186d7c93fedc24add16302c33b8b8614547fe65
Shivaprasad S Shetty <shivaprasadshetty112@gmail.com>
2023-10-17 21:53:56 +0530

    hulk design

--- ayurbot/src/App.js
+++ ayurbot/src/App.js
@@ -1,11 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Home from './screens/Home'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom"
 
 function App() {
   return (
-    <>
-    </>
-  );
+    <Router>
+      <div>
+        <Routes>
+          <Route exact path='/' element={<Home/>}/>
+        </Routes>
+      </div>
+    </Router>
+  )
 }
 
-export default App;
+export default App
---
+++ ayurbot/src/components/DoctorHome.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const DoctorHome = () => {
+  return (
+    <div className='bg-success'>
+        Doctor
+
+      
+    </div>
+  )
+}
+
+export default DoctorHome
